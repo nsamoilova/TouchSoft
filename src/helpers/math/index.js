8504cd3ddbe5abc48bcbe2c52f01981a31204592
nsamoilova <nsamoilov.an@gmail.com>
2021-11-23 18:58:22 +0200

    js

--- src/helpers/math/index.js
+++ src/helpers/math/index.js
@@ -11,30 +11,18 @@ function sayHello(text) {
 console.log(sayHello('Mark'));
 
 
-let arr = ['dk cd dch djhc','dchbd','aaaa','aaaa']
+let array = ['dkdjhc','dchbd','aaaa','aaaa']
 
 
 function filterStr () {
-  let arr = [];
-    for ( i = 0; i < arr[i].length; i++) {
-      if(arr[i].length < 5){
-        return true
-      }
-        
-    }
+  
 }
 
-console.log(filterStr(arr))
+let result = array.map(function(arr) {
+  return arr.length <= 4
+});
 
-// let result = arr.filter(function(a){
-//   arr.length = 5;
-//   return arr.indexOf(a.length) < 5;
-  
-// }); 
-
-// let result = arr.map(function(elem) {
-// 	return elem.length < 4;
-// });
+//  не понятно
 
 console.log(result);
 
