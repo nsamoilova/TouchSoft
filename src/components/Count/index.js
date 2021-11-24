55459c2cbc09d947dd6f3530755b2e2b666754e7
nsamoilova <nsamoilov.an@gmail.com>
2021-11-24 16:12:48 +0200

    react

--- src/components/Count/index.js
+++ src/components/Count/index.js
@@ -2,29 +2,27 @@ import React, { useState, useEffect } from 'react';
 import Input from 'components/Form/Input';
 import Button from 'components/Form/Button';
 import styles from './style.module.scss';
+import { values } from 'lodash';
 
 const Count = () => {
   const [count, setCount] = useState(0);
-  const [title, setTitle] = useState(()=>{
-    if(count % 2 == 0){
-      return 'Введено чётное число'
-      }else{
-       return 'Введено не четное число '
-    }
-    
-  });
+  const [title, setTitle] = useState('default')
 
   const [theme, setTheme] = useState()
  
- 
 
-//   useEffect(() => {
-//     if(count % 2 == 0){
-//       return 'чётное число'
-//     }else{
-//       return 'не чёт число'
-//     }
-// }, [title,count]);
+
+  useEffect(() => {
+     if(count % 2 == 0){
+      console.log('чётное')
+      
+     }else{
+      console.log('не чётное')
+     }
+   }, [count,title])
+
+
+
 
     return (
         <div className={styles.form}>
@@ -36,7 +34,7 @@ const Count = () => {
       <div className={styles.button}>
       <Button onClick={() => setCount(count - 1)} disabled={count === 0}>-</Button>
       <Button onClick={() => setCount(0)}>Reset</Button>
-      <Button onClick={() => setCount(count + 1)} disabled={count === 0}>+</Button >
+      <Button onClick={() => setCount(count + 1)} disabled={count === 0} >+</Button >
       </div>
         
     </div>
